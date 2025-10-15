33132224ffc5dfd6c8759e0fe067bc24fe14f36d
Magdy Shata <magdyshata262@gmail.com>
2025-10-15 14:40:38 +0300

    add-8

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+import { featureProductsRoutes } from '@myfeature-products/feature-products';
+export const appRoutes: Route[] = [
+
+      {
+    path: '',
+    redirectTo: 'products',
+    pathMatch: 'full'
+  },
+    {
+    path: 'products',
+    loadChildren: () => 
+      import('@myfeature-products/feature-products').then(m => m.FeatureProducts)
+  },
+];
