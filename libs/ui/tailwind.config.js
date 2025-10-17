3732108ef4f165e317fee3c0fb54716b83f34aff
Magdy Shata <magdyshata262@gmail.com>
2025-10-17 12:52:36 +0300

    add-26

--- libs/ui/tailwind.config.js
+++ libs/ui/tailwind.config.js
@@ -1,5 +1,3 @@
-// tailwind.config.js (في جذر المشروع D:\shop)
-
 const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
 const { join } = require('path');
 
@@ -25,13 +23,29 @@ module.exports = {
           900: '#0d47a1',
         },
         secondary: {
+          50: '#f8fafc',
+          100: '#f1f5f9',
+          200: '#e2e8f0',
+          300: '#cbd5e1',
+          400: '#94a3b8',
           500: '#64748b',
-          600: '#475569', // تأكد من وجود الدرجة 600
+          600: '#475569',
+          700: '#334155',
+          800: '#1e293b',
+          900: '#0f172a',
         },
-
-        success: '#4caf50',
-        warning: '#ff9800',
-        error: '#f44336',
+        gray: {
+          50: '#f9fafb',
+          100: '#f3f4f6',
+          200: '#e5e7eb',
+          300: '#d1d5db',
+          400: '#9ca3af',
+          500: '#6b7280',
+          600: '#4b5563',
+          700: '#374151',
+          800: '#1f2937',
+          900: '#111827',
+        }
       },
       spacing: {
         18: '4.5rem',
